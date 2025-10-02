1cfc6a0930af2bd9169ea8188eb9dc682278f6ea
Md Nasim Akhtar <Nasimjam89@gmail.com>
2025-10-02 19:19:41 +0530

    Add .gitignore for MERN project

---
+++ src/routes/short_url.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { createCustomShortUrl, createShortUrl } from '../controller/short_url.controller.js';
+const router = express.Router();
+
+router.post("/",createShortUrl);
+router.post("/",createCustomShortUrl);
+
+export default router;
